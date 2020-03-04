114d42d1749ba9b763c7bfefae10410f7f1dca34
Thiago Rios de Mello Gonçalves <thiago.mgoncalves@al.infnet.edu.br>
2020-03-04 08:59:46 -0300

    Avaliacao e filtro por nome

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,13 +13,27 @@
       Carrinho: {{ quantidadeCarrinho }} filmes
     </button>
     <div class="hhh" v-if="mostrarFilmes">
-      <div v-bind:key="filme.id" v-for="filme in filmes">
+      <div v-bind:key="filme.id" v-for="filme in filmesOrdenados">
         <div class="card">
           <img v-bind:src="filme.imagem" class="card-img-top" alt="imagem do filme">
           <div class="card-body">
             <h5 class="card-title">{{ filme.titulo }}</h5>
             <p class="card-text">{{ filme.descricao }}</p>
+            <span class="mensagem-estoque" v-if="filme.estoqueDisponivel - quantdadeCarinhoPorFilme(filme) === 0">
+              Indisponível
+            </span>
+            <span class="mensagem-estoque" v-else-if="filme.estoqueDisponivel - quantdadeCarinhoPorFilme(filme) < 5">
+              Apenas {{ filme.estoqueDisponivel - quantdadeCarinhoPorFilme(filme) }} itens disponíveis
+            </span>
+            <span class="mensagem-estoque" v-else>
+              Alugue agora
+            </span>
             <p class="card-text">{{ filme.valor | formatarValor("R$") }}</p>
+            <div class="avalicao">
+              <span v-for="n in 5" :key="n" v-bind:class="{'avaliacao-active': checarAvaliacao(n, filme) }">
+                <img src="../assets/estrela_vazia.jpg" height="20">
+              </span>
+            </div>
             <a href="#" @click="incrementar(filme)" v-if="validarAdd(filme)" class="btn btn-primary">ALUGAR</a>
             <a href="#" v-else class="btn btn-primary disabled">ALUGAR</a>
           </div>
@@ -45,10 +59,10 @@ export default {
       msg: "Locadora de Filmes (─‿─)",
       horas: new Date().getHours(),
       filmes: [
-        { id: 1, titulo: "Kimi no na wa", descricao: "Um filme de romance com viagem temporal", valor: 20, imagem: "https://s.aficionados.com.br/imagens/mv5bmza1mgjlzjatnwi1os00mtczlwe5n2ity2i5m2yxy2ziyjq0l2ltywdll2ltywdlxkeyxkfqcgdeqxvynzayodm5ode-v1-sx707-cr0-0-707-999-al_cke.jpg", estoqueDisponivel: 5},
-        { id: 2, titulo: "Tenki no Ko", descricao: "Um filme de romance", valor: 20, imagem: "https://img1.ak.crunchyroll.com/i/spire3/45e5866b5aed0f83bcfdb7565006b2451568165472_full.jpg", estoqueDisponivel: 7},
-        { id: 3, titulo: "Dragon Ball Z: A Batalha dos Deuses", descricao: "Um filme de porrada", valor: 20, imagem: "https://br.web.img3.acsta.net/pictures/210/456/21045654_20131001150454933.jpg", estoqueDisponivel: 3},
-        { id: 4, titulo: "Bunny Girl", descricao: "Um filme de romance e misterio", valor: 20, imagem: "https://img1.ak.crunchyroll.com/i/spire2/6eb7cc17a5b2ef8ffa11ae011fd7d3971555765943_full.jpg", estoqueDisponivel: 0}
+        { id: 1, titulo: "Kimi no na wa", descricao: "Um filme de romance com viagem temporal", valor: 20, imagem: "https://s.aficionados.com.br/imagens/mv5bmza1mgjlzjatnwi1os00mtczlwe5n2ity2i5m2yxy2ziyjq0l2ltywdll2ltywdlxkeyxkfqcgdeqxvynzayodm5ode-v1-sx707-cr0-0-707-999-al_cke.jpg", estoqueDisponivel: 5, avaliacao: 5},
+        { id: 2, titulo: "Tenki no Ko", descricao: "Um filme de romance", valor: 20, imagem: "https://img1.ak.crunchyroll.com/i/spire3/45e5866b5aed0f83bcfdb7565006b2451568165472_full.jpg", estoqueDisponivel: 7, avaliacao: 3},
+        { id: 3, titulo: "Dragon Ball Z: A Batalha dos Deuses", descricao: "Um filme de porrada", valor: 20, imagem: "https://br.web.img3.acsta.net/pictures/210/456/21045654_20131001150454933.jpg", estoqueDisponivel: 3, avaliacao: 4},
+        { id: 4, titulo: "Bunny Girl", descricao: "Um filme de romance e misterio", valor: 20, imagem: "https://img1.ak.crunchyroll.com/i/spire2/6eb7cc17a5b2ef8ffa11ae011fd7d3971555765943_full.jpg", estoqueDisponivel: 0, avaliacao: 4}
       ],
       carrinho: []
     }
@@ -78,11 +92,26 @@ export default {
     },
     validarAdd: function(filme) {
       return filme.estoqueDisponivel > this.quantdadeCarinhoPorFilme(filme)
+    },
+    checarAvaliacao(n, filme) {
+      return filme.avaliacao - n >= 0;
     }
   }, computed: {
     quantidadeCarrinho: function() {
       return this.carrinho.length
-    }
+    },
+    filmesOrdenados: function(){
+      let filmesOrd = this.filmes
+      filmesOrd.sort(function (a, b) {
+        if (a.titulo > b.titulo) {
+          return 1;
+        }
+        if (a.titulo < b.titulo) {
+          return -1;
+        }
+        return 0;
+      });
+      return this.filmes    }
   }
 }
 </script>
@@ -111,9 +140,17 @@ li {
 #teste1{
   flex-direction: column;
 }
+.mensagem-estoque{
+  font-weight: bold;
+}
 .card {
   width: 300px;
-  height: 650px;
+  height: 100%;
   margin-top: 5px;
+}
+span.avaliacao-active {
+  background-image: url("../assets/estrela_marcada.jpg");
+  background-repeat: no-repeat;
+  background-size: 20px; 
 }
 </style>
